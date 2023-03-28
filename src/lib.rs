3c4766668b2706cc2be8a48fa5beddd7fa27425f
Marcin Kulik <m@ku1ik.com>
2023-03-28 14:08:53 +0200

    Move logic to lib crate, keep cli in main

---
+++ src/lib.rs
@@ -0,0 +1,214 @@
+use anyhow::{anyhow, Result};
+use clap::ArgEnum;
+use log::info;
+use std::fmt::{Debug, Display};
+use std::{fs::File, thread, time::Instant};
+mod asciicast;
+mod events;
+mod fonts;
+mod renderer;
+mod theme;
+mod vt;
+
+pub const DEFAULT_FONT_FAMILY: &str =
+    "JetBrains Mono,Fira Code,SF Mono,Menlo,Consolas,DejaVu Sans Mono,Liberation Mono";
+pub const DEFAULT_FONT_SIZE: usize = 14;
+pub const DEFAULT_FPS_CAP: u8 = 30;
+pub const DEFAULT_LAST_FRAME_DURATION: f64 = 3.0;
+pub const DEFAULT_LINE_HEIGHT: f64 = 1.4;
+pub const DEFAULT_NO_LOOP: bool = false;
+pub const DEFAULT_SPEED: f64 = 1.0;
+
+pub struct Config {
+    pub cols: Option<usize>,
+    pub font_dirs: Vec<String>,
+    pub font_family: String,
+    pub font_size: usize,
+    pub fps_cap: u8,
+    pub idle_time_limit: Option<f64>,
+    pub last_frame_duration: f64,
+    pub line_height: f64,
+    pub no_loop: bool,
+    pub renderer: Renderer,
+    pub rows: Option<usize>,
+    pub speed: f64,
+    pub theme: Option<Theme>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            cols: None,
+            font_dirs: vec![],
+            font_family: String::from(DEFAULT_FONT_FAMILY),
+            font_size: DEFAULT_FONT_SIZE,
+            fps_cap: DEFAULT_FPS_CAP,
+            idle_time_limit: None,
+            last_frame_duration: DEFAULT_LAST_FRAME_DURATION,
+            line_height: DEFAULT_LINE_HEIGHT,
+            no_loop: DEFAULT_NO_LOOP,
+            renderer: Default::default(),
+            rows: None,
+            speed: DEFAULT_SPEED,
+            theme: Default::default(),
+        }
+    }
+}
+
+#[derive(Clone, ArgEnum)]
+pub enum Renderer {
+    Fontdue,
+    Resvg,
+}
+
+impl Default for Renderer {
+    fn default() -> Self {
+        Renderer::Fontdue
+    }
+}
+
+#[derive(Clone, Debug, ArgEnum)]
+pub enum Theme {
+    Asciinema,
+    Dracula,
+    Monokai,
+    SolarizedDark,
+    SolarizedLight,
+
+    #[clap(skip)]
+    Custom(String),
+    #[clap(skip)]
+    Embedded(theme::Theme),
+}
+
+impl Default for Theme {
+    fn default() -> Self {
+        Theme::Dracula
+    }
+}
+
+impl TryFrom<Theme> for theme::Theme {
+    type Error = anyhow::Error;
+
+    fn try_from(theme: Theme) -> std::result::Result<Self, Self::Error> {
+        use Theme::*;
+
+        match theme {
+            Asciinema => "121314,cccccc,000000,dd3c69,4ebf22,ddaf3c,26b0d7,b954e1,54e1b9,d9d9d9,4d4d4d,dd3c69,4ebf22,ddaf3c,26b0d7,b954e1,54e1b9,ffffff".parse(),
+            Dracula => "282a36,f8f8f2,21222c,ff5555,50fa7b,f1fa8c,bd93f9,ff79c6,8be9fd,f8f8f2,6272a4,ff6e6e,69ff94,ffffa5,d6acff,ff92df,a4ffff,ffffff".parse(),
+            Monokai => "272822,f8f8f2,272822,f92672,a6e22e,f4bf75,66d9ef,ae81ff,a1efe4,f8f8f2,75715e,f92672,a6e22e,f4bf75,66d9ef,ae81ff,a1efe4,f9f8f5".parse(),
+            SolarizedDark => "002b36,839496,073642,dc322f,859900,b58900,268bd2,d33682,2aa198,eee8d5,002b36,cb4b16,586e75,657b83,839496,6c71c4,93a1a1,fdf6e3".parse(),
+            SolarizedLight => "fdf6e3,657b83,073642,dc322f,859900,b58900,268bd2,d33682,2aa198,eee8d5,002b36,cb4b16,586e75,657c83,839496,6c71c4,93a1a1,fdf6e3".parse(),
+            Custom(t) => t.parse(),
+            Embedded(t) => Ok(t),
+        }
+    }
+}
+
+impl Display for Theme {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Theme::*;
+
+        match self {
+            Custom(_) => f.write_str("custom"),
+            Embedded(_) => f.write_str("embedded"),
+            t => write!(f, "{}", format!("{:?}", t).to_lowercase()),
+        }
+    }
+}
+
+pub fn run(input_filename: &str, output_filename: &str, config: Config) -> Result<()> {
+    let (header, events) = asciicast::open(input_filename)?;
+
+    let terminal_size = (
+        config.cols.unwrap_or(header.terminal_size.0),
+        config.rows.unwrap_or(header.terminal_size.1),
+    );
+
+    let itl = config
+        .idle_time_limit
+        .or(header.idle_time_limit)
+        .unwrap_or(5.0);
+
+    let stdout = asciicast::stdout(events);
+    let stdout = events::limit_idle_time(stdout, itl);
+    let stdout = events::accelerate(stdout, config.speed);
+    let stdout = events::batch(stdout, config.fps_cap);
+    let stdout = stdout.collect::<Vec<_>>();
+    let count = stdout.len() as u64;
+    let frames = vt::frames(stdout.into_iter(), terminal_size);
+
+    info!("terminal size: {}x{}", terminal_size.0, terminal_size.1);
+
+    let (font_db, font_family) = fonts::init(&config.font_dirs, &config.font_family)
+        .ok_or_else(|| anyhow!("no faces matching font family {}", config.font_family))?;
+
+    info!("selected font family: {}", &font_family);
+
+    let theme_opt = config
+        .theme
+        .or_else(|| header.theme.map(Theme::Embedded))
+        .unwrap_or(Theme::Dracula);
+
+    info!("selected theme: {}", theme_opt);
+
+    let settings = renderer::Settings {
+        terminal_size,
+        font_db,
+        font_family,
+        font_size: config.font_size,
+        line_height: config.line_height,
+        theme: theme_opt.try_into()?,
+    };
+
+    let mut renderer: Box<dyn renderer::Renderer> = match config.renderer {
+        Renderer::Fontdue => Box::new(renderer::fontdue(settings)),
+        Renderer::Resvg => Box::new(renderer::resvg(settings)),
+    };
+
+    let (width, height) = renderer.pixel_size();
+
+    info!("gif dimensions: {}x{}", width, height);
+
+    let repeat = if config.no_loop {
+        gifski::Repeat::Finite(0)
+    } else {
+        gifski::Repeat::Infinite
+    };
+
+    let settings = gifski::Settings {
+        width: Some(width as u32),
+        height: Some(height as u32),
+        fast: true,
+        repeat,
+        ..Default::default()
+    };
+
+    let (collector, writer) = gifski::new(settings)?;
+    let start_time = Instant::now();
+    let file = File::create(output_filename)?;
+
+    let writer_handle = thread::spawn(move || {
+        let mut pr = gifski::progress::ProgressBar::new(count);
+        let result = writer.write(file, &mut pr);
+        pr.finish();
+
+        result
+    });
+
+    for (i, (time, lines, cursor)) in frames.enumerate() {
+        let image = renderer.render(lines, cursor);
+        let time = if i == 0 { 0.0 } else { time };
+        collector.add_frame_rgba(i, image, time + config.last_frame_duration)?;
+    }
+
+    drop(collector);
+    writer_handle.join().unwrap()?;
+
+    info!(
+        "rendering finished in {}s",
+        start_time.elapsed().as_secs_f32()
+    );
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,15 @@
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use clap::{ArgAction, ArgEnum, Parser};
-use log::info;
-use std::fmt::{Debug, Display};
 use std::iter;
-use std::{fs::File, thread, time::Instant};
-mod asciicast;
-mod events;
-mod fonts;
-mod renderer;
-mod theme;
-mod vt;
-use crate::renderer::Renderer;
-use crate::theme::Theme;
-
-#[derive(Clone, ArgEnum)]
-enum RendererOpt {
-    Fontdue,
-    Resvg,
-}
-
-#[derive(Clone, Debug, ArgEnum)]
-pub enum BuiltinTheme {
-    Asciinema,
-    Dracula,
-    Monokai,
-    SolarizedDark,
-    SolarizedLight,
-}
 
 #[derive(Clone)]
-pub enum ThemeOpt {
-    Builtin(BuiltinTheme),
-    Custom(Theme),
-    Embedded(Theme),
-}
+pub struct Theme(agg::Theme);
 
-impl From<ThemeOpt> for Theme {
-    fn from(theme_opt: ThemeOpt) -> Self {
-        use BuiltinTheme::*;
-        use ThemeOpt::*;
+#[derive(Clone)]
+pub struct ThemeValueParser;
 
-        match theme_opt {
-            Builtin(Asciinema) => "121314,cccccc,000000,dd3c69,4ebf22,ddaf3c,26b0d7,b954e1,54e1b9,d9d9d9,4d4d4d,dd3c69,4ebf22,ddaf3c,26b0d7,b954e1,54e1b9,ffffff".parse().unwrap(),
-            Builtin(Dracula) => "282a36,f8f8f2,21222c,ff5555,50fa7b,f1fa8c,bd93f9,ff79c6,8be9fd,f8f8f2,6272a4,ff6e6e,69ff94,ffffa5,d6acff,ff92df,a4ffff,ffffff".parse().unwrap(),
-            Builtin(Monokai) => "272822,f8f8f2,272822,f92672,a6e22e,f4bf75,66d9ef,ae81ff,a1efe4,f8f8f2,75715e,f92672,a6e22e,f4bf75,66d9ef,ae81ff,a1efe4,f9f8f5".parse().unwrap(),
-            Builtin(SolarizedDark) => "002b36,839496,073642,dc322f,859900,b58900,268bd2,d33682,2aa198,eee8d5,002b36,cb4b16,586e75,657b83,839496,6c71c4,93a1a1,fdf6e3".parse().unwrap(),
-            Builtin(SolarizedLight) => "fdf6e3,657b83,073642,dc322f,859900,b58900,268bd2,d33682,2aa198,eee8d5,002b36,cb4b16,586e75,657c83,839496,6c71c4,93a1a1,fdf6e3".parse().unwrap(),
-            Custom(t) => t,
-            Embedded(t) => t,
-        }
-    }
-}
-
-impl Display for ThemeOpt {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        use ThemeOpt::*;
-
-        match self {
-            Builtin(t) => write!(f, "{}", format!("{:?}", t).to_lowercase()),
-            Custom(_) => f.write_str("custom"),
-            Embedded(_) => f.write_str("embedded"),
-        }
-    }
-}
-
-impl clap::builder::ValueParserFactory for ThemeOpt {
-    type Parser = ThemeOptValueParser;
-
-    fn value_parser() -> Self::Parser {
-        ThemeOptValueParser
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct ThemeOptValueParser;
-
-impl clap::builder::TypedValueParser for ThemeOptValueParser {
-    type Value = ThemeOpt;
+impl clap::builder::TypedValueParser for ThemeValueParser {
+    type Value = Theme;
 
     fn parse_ref(
         &self,
@@ -87,23 +20,11 @@ impl clap::builder::TypedValueParser for ThemeOptValueParser {
         let s = value.to_string_lossy();
 
         if s.contains(',') {
-            match s.parse() {
-                Ok(t) => Ok(ThemeOpt::Custom(t)),
-
-                Err(e) => {
-                    let mut cmd = cmd.clone();
-                    let e = cmd.error(
-                        clap::ErrorKind::ValueValidation,
-                        format!("invalid theme definition: {}", e),
-                    );
-
-                    Err(e.format(&mut cmd))
-                }
-            }
+            Ok(Theme(agg::Theme::Custom(s.to_string())))
         } else {
-            let inner = clap::value_parser!(BuiltinTheme);
-
-            inner.parse_ref(cmd, arg, value).map(ThemeOpt::Builtin)
+            clap::value_parser!(agg::Theme)
+                .parse_ref(cmd, arg, value)
+                .map(Theme)
         }
     }
 
@@ -111,7 +32,7 @@ impl clap::builder::TypedValueParser for ThemeOptValueParser {
         &self,
     ) -> Option<Box<dyn Iterator<Item = clap::PossibleValue<'static>> + '_>> {
         Some(Box::new(
-            BuiltinTheme::value_variants()
+            agg::Theme::value_variants()
                 .iter()
                 .filter_map(|v| v.to_possible_value())
                 .chain(iter::once(clap::PossibleValue::new("custom"))),
@@ -129,35 +50,35 @@ struct Cli {
     output_filename: String,
 
     /// Select frame rendering backend
-    #[clap(long, arg_enum, default_value_t = RendererOpt::Fontdue)]
-    renderer: RendererOpt,
+    #[clap(long, arg_enum, default_value_t = agg::Renderer::default())]
+    renderer: agg::Renderer,
 
     /// Specify font family
-    #[clap(long, default_value_t = String::from("JetBrains Mono,Fira Code,SF Mono,Menlo,Consolas,DejaVu Sans Mono,Liberation Mono"))]
+    #[clap(long, default_value_t = String::from(agg::DEFAULT_FONT_FAMILY))]
     font_family: String,
 
     /// Specify font size (in pixels)
-    #[clap(long, default_value_t = 14)]
+    #[clap(long, default_value_t = agg::DEFAULT_FONT_SIZE)]
     font_size: usize,
 
     /// Specify line height
-    #[clap(long, default_value_t = 1.4)]
+    #[clap(long, default_value_t = agg::DEFAULT_LINE_HEIGHT)]
     line_height: f64,
 
     /// Select color theme
-    #[clap(long, value_parser = ThemeOptValueParser)]
-    theme: Option<ThemeOpt>,
+    #[clap(long, value_parser = ThemeValueParser)]
+    theme: Option<Theme>,
 
     /// Use additional font directory
     #[clap(long)]
     font_dir: Vec<String>,
 
     /// Adjust playback speed
-    #[clap(long, default_value_t = 1.0)]
+    #[clap(long, default_value_t = agg::DEFAULT_SPEED)]
     speed: f64,
 
     /// Disable animation loop
-    #[clap(long)]
+    #[clap(long, default_value_t = agg::DEFAULT_NO_LOOP)]
     no_loop: bool,
 
     /// Limit idle time to max number of seconds [default: 5]
@@ -165,11 +86,11 @@ struct Cli {
     idle_time_limit: Option<f64>,
 
     /// Set FPS cap
-    #[clap(long, default_value_t = 30)]
+    #[clap(long, default_value_t = agg::DEFAULT_FPS_CAP)]
     fps_cap: u8,
 
     /// Set last frame duration
-    #[clap(long, default_value_t = 3.0)]
+    #[clap(long, default_value_t = agg::DEFAULT_LAST_FRAME_DURATION)]
     last_frame_duration: f64,
 
     /// Override terminal width (number of columns)
@@ -196,99 +117,21 @@ fn main() -> Result<()> {
 
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(log_level)).init();
 
-    let (header, events) = asciicast::open(&cli.input_filename)?;
-
-    let terminal_size = (
-        cli.cols.unwrap_or(header.terminal_size.0),
-        cli.rows.unwrap_or(header.terminal_size.1),
-    );
-
-    let itl = cli
-        .idle_time_limit
-        .or(header.idle_time_limit)
-        .unwrap_or(5.0);
-
-    let stdout = asciicast::stdout(events);
-    let stdout = events::limit_idle_time(stdout, itl);
-    let stdout = events::accelerate(stdout, cli.speed);
-    let stdout = events::batch(stdout, cli.fps_cap);
-    let stdout = stdout.collect::<Vec<_>>();
-    let count = stdout.len() as u64;
-    let frames = vt::frames(stdout.into_iter(), terminal_size);
-
-    info!("terminal size: {}x{}", terminal_size.0, terminal_size.1);
-
-    let (font_db, font_family) = fonts::init(&cli.font_dir, &cli.font_family)
-        .ok_or_else(|| anyhow!("no faces matching font family {}", cli.font_family))?;
-
-    info!("selected font family: {}", &font_family);
-
-    let theme_opt = cli
-        .theme
-        .or_else(|| header.theme.map(ThemeOpt::Embedded))
-        .unwrap_or(ThemeOpt::Builtin(BuiltinTheme::Dracula));
-
-    info!("selected theme: {}", theme_opt);
-
-    let theme: Theme = theme_opt.into();
-
-    let settings = renderer::Settings {
-        terminal_size,
-        font_db,
-        font_family,
+    let config = agg::Config {
+        cols: cli.cols,
+        font_dirs: cli.font_dir,
+        font_family: cli.font_family,
         font_size: cli.font_size,
+        fps_cap: cli.fps_cap,
+        idle_time_limit: cli.idle_time_limit,
+        last_frame_duration: cli.last_frame_duration,
         line_height: cli.line_height,
-        theme,
+        no_loop: cli.no_loop,
+        renderer: cli.renderer,
+        rows: cli.rows,
+        speed: cli.speed,
+        theme: cli.theme.map(|theme| theme.0),
     };
 
-    let mut renderer: Box<dyn Renderer> = match cli.renderer {
-        RendererOpt::Fontdue => Box::new(renderer::fontdue(settings)),
-        RendererOpt::Resvg => Box::new(renderer::resvg(settings)),
-    };
-
-    let (width, height) = renderer.pixel_size();
-
-    info!("gif dimensions: {}x{}", width, height);
-
-    let repeat = if cli.no_loop {
-        gifski::Repeat::Finite(0)
-    } else {
-        gifski::Repeat::Infinite
-    };
-
-    let settings = gifski::Settings {
-        width: Some(width as u32),
-        height: Some(height as u32),
-        fast: true,
-        repeat,
-        ..Default::default()
-    };
-
-    let (collector, writer) = gifski::new(settings)?;
-    let start_time = Instant::now();
-    let file = File::create(cli.output_filename)?;
-
-    let writer_handle = thread::spawn(move || {
-        let mut pr = gifski::progress::ProgressBar::new(count);
-        let result = writer.write(file, &mut pr);
-        pr.finish();
-
-        result
-    });
-
-    for (i, (time, lines, cursor)) in frames.enumerate() {
-        let image = renderer.render(lines, cursor);
-        let time = if i == 0 { 0.0 } else { time };
-        collector.add_frame_rgba(i, image, time + cli.last_frame_duration)?;
-    }
-
-    drop(collector);
-    writer_handle.join().unwrap()?;
-
-    info!(
-        "rendering finished in {}s",
-        start_time.elapsed().as_secs_f32()
-    );
-
-    Ok(())
+    agg::run(&cli.input_filename, &cli.output_filename, config)
 }
