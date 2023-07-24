22349f46924f0a6e8f6bb878a7ac6a5cf2b3235d
Yvan Sraka <yvan@sraka.xyz>
2023-07-24 11:17:50 +0200

    Fixes #48: unable to `nix run`
        error: unable to execute '/nix/store/.../bin/agg-1.4.2': No such file or directory

--- flake.nix
+++ flake.nix
@@ -4,14 +4,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
       in {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = naersk-lib.buildPackage {
+          pname = "agg";
+          src = ./.;
+        };
         defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
         devShell = with pkgs;
           mkShell {
