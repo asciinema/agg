fed64cf6b394e457d8cfef4a421f5e00aefa102a
Marcin Kulik <m@ku1ik.com>
2026-04-30 15:07:05 +0200

    Add MSRV dev shell

--- flake.nix
+++ flake.nix
@@ -2,20 +2,44 @@
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     utils.url = "github:numtide/flake-utils";
   };
   outputs =
     {
       self,
       nixpkgs,
+      rust-overlay,
       utils,
       naersk,
     }:
+    let
+      packageToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package;
+      msrv = packageToml.rust-version;
+    in
     utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
         naersk-lib = pkgs.callPackage naersk { };
+        mkDevShell =
+          rust:
+          pkgs.mkShell {
+            nativeBuildInputs = [
+              (rust.override {
+                extensions = [
+                  "rustfmt"
+                  "rust-src"
+                  "rust-analyzer"
+                  "clippy"
+                ];
+              })
+              pkgs.pre-commit
+            ];
+          };
       in
       {
         defaultPackage = naersk-lib.buildPackage {
@@ -25,20 +49,8 @@
 
         defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
 
-        devShell =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              cargo
-              rustc
-              rustfmt
-              rust-analyzer
-              pre-commit
-              rustPackages.clippy
-            ];
-
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          };
+        devShells.default = mkDevShell pkgs.rust-bin.stable.latest.default;
+        devShells.msrv = mkDevShell pkgs.rust-bin.stable.${msrv}.default;
       }
     );
 }
